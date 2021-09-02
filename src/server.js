04c163bf039aca53d8496dd8f006950476efb557
starhn87 <starhn87@gmail.com>
2021-09-02 22:58:46 +0900

    install morgan

--- src/server.js
+++ src/server.js
@@ -1,22 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res, next) => {
   return res.send("I love middleware.");
@@ -31,8 +19,6 @@ const handleLogin = (req, res) => {
 };
 
 app.use(logger);
-// app.use(privateMiddleware);
-
 app.get("/", handleHome);
 app.get("/protected", handleProtected);
 app.get("/login", handleLogin);
