bcbd28c585985a1b11eb756968d57f53c3469a5c
starhn87 <starhn87@gmail.com>
2021-09-05 19:28:46 +0900

    introduce pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,6 @@
-export const trending = (req, res) => res.send("Homepage Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send("Watch Video");
-};
-export const edit = (req, res) => res.send("Edit Video");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
