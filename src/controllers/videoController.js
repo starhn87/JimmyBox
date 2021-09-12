d3709afa71a331da6d33929720a85edd849faaf9
starhn87 <starhn87@gmail.com>
2021-09-12 21:52:48 +0900

    start db manipulation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,23 +1,35 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  console.log("start");
-  Video.find({}, (error, videos) => {
-    console.log("Finished");
-    return res.render("home", { pageTitle: "Home", videos: [] });
-  });
+// Video.find({}, (error, videos) => {
+//   if (error) {
+//     return res.render("server error");
+//   }
+//   return res.render("home", { pageTitle: "Home", videos });
+// });
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    return res.render("server error", { error });
+  }
 };
-export const watch = (req, res) => {
+
+export const watch = async (req, res) => {
   const { id } = req.params;
-  console.log(`id: ${id}`);
+  const video = await Video.findById(id);
   return res.render("watch", {
-    pageTitle: "Watch",
+    pageTitle: video.title,
+    video,
   });
 };
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
   return res.render("edit", { pageTitle: "Edit" });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -28,7 +40,19 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
