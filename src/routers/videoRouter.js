7acd39f30697babb9bd55aa72192c0ae9e2e793b
starhn87 <starhn87@gmail.com>
2021-09-14 23:47:46 +0900

    Video CRUD

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,12 +5,14 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.route("/:id([0-9a-f]{24})").get(watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
