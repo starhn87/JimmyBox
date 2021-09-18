a0183210472e7e6a1fb042384012d6ac044df74a
starhn87 <starhn87@gmail.com>
2021-09-18 21:56:06 +0900

    make join and login with password hash

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,7 +22,7 @@ export const watch = async (req, res) => {
   console.log(video);
 
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
 
   return res.render("watch", {
@@ -36,7 +36,7 @@ export const getEdit = async (req, res) => {
   const video = await Video.findById(id);
 
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
 
   return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
@@ -48,7 +48,7 @@ export const postEdit = async (req, res) => {
   const video = await Video.exists({ _id: id });
 
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
 
   await Video.findByIdAndUpdate(id, {
@@ -75,7 +75,7 @@ export const postUpload = async (req, res) => {
 
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
