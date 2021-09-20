921342bc704cca0d2fc76018aff85a3351271235
starhn87 <starhn87@gmail.com>
2021-09-20 21:26:39 +0900

    user authentication

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { remove, edit, see, logout } from "../controllers/userController";
+import {
+  edit,
+  see,
+  logout,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
